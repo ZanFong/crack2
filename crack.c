306d9b6134cff5dad21ebf7f763698da8c7d8727
Zander Fong <etorbik@student.sierracollege.edu>
2024-10-27 20:33:42 -0700

    Completed file for Level 9 assignment

--- crack.c
+++ crack.c
@@ -4,67 +4,103 @@
 
 #include "md5.h"
 
+//provided string lengths
 const int PASS_LEN = 20;        // Maximum any password will be
 const int HASH_LEN = 33;        // Length of MD5 hash strings
 
-
+//provided function outline
 // Given a target plaintext word, use it to try to find
 // a matching hash in the hashFile.
-// Get this function working first!
 char * tryWord(char * plaintext, char * hashFilename)
 {
-    // Hash the plaintext
+    //store hash of provided plaintext password
+    char *password = md5(plaintext, strlen(plaintext));
 
     // Open the hash file
+    FILE *hashFile = fopen(hashFilename, "r");
+    if(!hashFile) 
+    {
+        printf("Unable to open file %s!\n", hashFilename);
+        exit(1);
+    }
+
+    //init var to store hash value from file
+    char hash[HASH_LEN];
 
     // Loop through the hash file, one line at a time.
+    while(fgets(hash, HASH_LEN, hashFile))
+    {
+        //trim newline if needed
+        if(hash[strlen(hash) - 1] == '\n') hash[strlen(hash) - 1] = '\0';
 
-    // Attempt to match the hash from the file to the
-    // hash of the plaintext.
+        //compare password hash to hash list
+        //if match, return hash
+        if(!strcmp(hash, password))
+        {
+            //free memory and close file
+            free(password);
+            fclose(hashFile);
 
-    // If there is a match, you'll return the hash.
-    // If not, return NULL.
+            return hash;
+        }
+    }
 
-    // Before returning, do any needed cleanup:
-    //   Close files?
-    //   Free memory?
+    //free memory and close file
+    free(password);
+    fclose(hashFile);
 
-    // Modify this line so it returns the hash
-    // that was found, or NULL if not found.
-    return "0123456789abcdef0123456789abcdef";
+    //return NULL if no match found
+    return NULL;
 }
 
-
+//main function
+//takes 2 arguments from the command line
 int main(int argc, char *argv[])
-{
+{   
+    //if too few arguments, exit
     if (argc < 3) 
     {
         fprintf(stderr, "Usage: %s hash_file dict_file\n", argv[0]);
         exit(1);
     }
 
-    // These two lines exist for testing. When you have
-    // tryWord working, it should display the hash for "hello",
-    // which is 5d41402abc4b2a76b9719d911017c592.
-    // Then you can remove these two lines and complete the rest
-    // of the main function below.
-    char *found = tryWord("hello", "hashes00.txt");
-    printf("%s %s\n", found, "hello");
+    //open dictionary file
+    FILE *dict = fopen(argv[2], "r");
+    //check that it opened correctly
+    if(!dict)
+    {
+        printf("Unable to open file %s!\n", argv[2]); //if not, error and exit
+        exit(1);
+    }
 
+    //init string to hold dictionary words
+    char dictWord[PASS_LEN];
+    //init var to hold number of successful matches
+    int hacked = 0;
 
-    // Open the dictionary file for reading.
-    
+    //for the length of the dictionary file
+    while(fgets(dictWord, PASS_LEN, dict))
+    {  
+        //trim newline if needed
+        if(dictWord[strlen(dictWord) - 1] == '\n') dictWord[strlen(dictWord) - 1] = '\0';
+        
+        //store result of tryWord
+        char *match = tryWord(dictWord, argv[1]);
+        
+        //if tryWord returned non-NULL value
+        //print the hash and matching dictionary word
+        //increment hacked
+        if(match)
+        {
+            printf("%s %s\n", match, dictWord); 
+            hacked++;
+        }
+    }
 
-    // For each dictionary word, pass it to tryWord, which
-    // will attempt to match it against the hashes in the hash_file.
-    
-    // If we got a match, display the hash and the word. For example:
-    //   5d41402abc4b2a76b9719d911017c592 hello
-    
-    // Close the dictionary file.
+    //close file 
+    fclose(dict);
 
-    // Display the number of hashes that were cracked.
-    
-    // Free up any malloc'd memory?
+    //print total number of successful matches
+    printf("%d hashes were cracked.\n", hacked);
 }
 
